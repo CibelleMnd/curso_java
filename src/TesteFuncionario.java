172b902404c08528fb70ef79c436e729a29c7e58
Cibele <bell.mnd@outlook.com>
2020-05-17 00:04:11 -0300

    first commit

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,24 @@
+
+public class TesteFuncionario {
+	
+
+	public static void main(String[] args) {
+		
+		
+		Funcionario nico = new Funcionario();
+		
+		nico.setNome("Nico Steppat");
+		nico.setCpf("223355646");
+		nico.setSalario(2600.00);
+		
+
+		
+		System.out.println(nico.getNome());
+		System.out.println(nico.getBonificacao());
+		
+		
+		
+
+	}
+
+}
